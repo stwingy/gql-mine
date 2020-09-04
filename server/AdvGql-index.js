08d72244ea2e1fa3ac7d9d5e078085b1ebf4ad5f
stwingy <john@scallywag.org.uk>
2020-09-04 20:21:03 +0100

    union & interfaces

--- server/AdvGql-index.js
+++ server/AdvGql-index.js
@@ -1,6 +1,100 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+var interfaceEG =`
+{
+	tablet(input:ipad){
+	  type
+	  browser
+	  ... on IpadPro{
+		appleGuard
+	  }
+		 }
+	tab2:tablet(input:kindle){
+	  type
+	  browser
+	  ... on Kindle{
+		reader
+	  }
+		 }
+	 tab3:tablet(input:nexus){
+	  type
+	  browser
+	  ... on Nexus2{
+	   control
+	  }
+		 }
+  }
+OUTPUTS
+{
+	"data": {
+	  "tablet": {
+		"type": "ipad",
+		"browser": "Chrome",
+		"appleGuard": 3
+	  },
+	  "tab2": {
+		"type": "kindle",
+		"browser": "Chrome",
+		"reader": "Acrobat"
+	  },
+	  "tab3": {
+		"type": "nexus",
+		"browser": "Chrome",
+		"control": true
+	  }
+	}
+  }
+  MORE EFFICENT 3 fragments
+  {
+	tablet(input:nexus){
+	  type
+	  browser
+	  ... on IpadPro{
+		appleGuard
+	  }
+	  ... on Kindle{
+		reader
+	  }
+	  ... on Nexus2{
+	   control
+	  }
+		 }
+   
+  }
+  outputs
+  {
+	"data": {
+	  "tablet": {
+		"type": "nexus",
+		"browser": "Chrome",
+		"control": true
+	  }
+	}
+  }
 
+  UNION
+  {
+	searchResult(id:"1"){
+	  ...on Human{
+		age
+		name
+	  }
+	  ...on Animal{
+	  species
+	}
+	}
+  }
+  OUTPUT
+  {
+	"data": {
+	  "searchResult": [
+		{
+		  "species": "dog"
+		}
+	  ]
+	}
+  }
+`
 const typeDefs = gql`
 # enums work on type or input
 enum GenderTypes{  
@@ -23,11 +117,43 @@ enum GenderTypes{
 
 	type Animal {
 		id: ID!
-		species: String!
+		species: String
 	}
 
+	union SearchResult = Human | Animal
+# unions & interfaces allow us to group together different types
+#unions -returns many different types with nothing in common, can only be custom types
+# whatever is in the interface must be in the type as well
+# __resolveType has no args =>parent,ctx,path    resolves to type as String eg "IpadPro"
+enum TabletType{
+	ipad
+	kindle
+	nexus
+}
+interface Tablet{
+browser:String	
+type:TabletType
+}
+type IpadPro implements Tablet{
+	browser:String
+	type:TabletType
+	appleGuard:Int
+}
+type Kindle implements Tablet{
+	browser:String
+	type:TabletType
+	reader:String
+}
+type Nexus2 implements Tablet{
+	browser:String
+	type:TabletType
+	control:Boolean
+}
 	type Query {
+		searchResult(id:ID!):[SearchResult]
+		tablet(input:TabletType):Tablet!
 		human(id: ID!): Human
+		animal(id: ID!): Animal
 		allHumans: [Human]!
 	}
 
@@ -38,6 +164,23 @@ enum GenderTypes{
 
 const resolvers = {
 	Query: {
+		searchResult(parent,args){
+let Array =[]
+const a = myArray.find((a) => a.id === args.id);
+const b =animalArray.find((a) => a.id === args.id);
+if(a) Array.push(a)
+if(b) Array.push(b)
+return Array
+		},
+		tablet(parent,args,ctx){
+return{
+	type:args.input,
+	browser:"Chrome",
+	reader:"Acrobat",
+	appleGuard:3,
+	control:true
+}
+		},
 		human(parent, args, ctx) {
 			const { id, name, pet,age,gender } = myArray.find((a) => a.id === args.id);
 
@@ -51,7 +194,12 @@ const resolvers = {
 		},
 		allHumans(parent, args, ctx) {
 			return ctx.array;
-		}
+		},
+		animal(parent,args){
+			const { id, species } = animalArray.find((a) => a.id === args.id);
+			return { id, species };
+		},
+		
 	},
 	Human: {
 		pet(parent, args) {
@@ -61,6 +209,27 @@ const resolvers = {
 			const { id, species } = animalArray.find((a) => a.id === pet);
 			return { id, species };
 		}
+	},
+	SearchResult:{
+__resolveType(parent){
+	if(parent.species){
+		return "Animal"
+	}else{
+		return "Human"
+	}
+}
+	},
+	Tablet:{
+__resolveType(parent,ctx){
+switch(parent.type){
+	case "ipad":
+		return "IpadPro"
+	case "kindle":
+		return "Kindle"
+	default:
+		return "Nexus2"
+}
+}
 	},
 	Mutation: {
 		newHuman(parent, args, ctx) {
